7f9bc1e6442db4dcc2bb3a51c6ac590972743160
Michael Clermont <michael.clermont@mths.ca>
2022-09-20 20:55:59 +0000

    added rock program

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+/**
+ * The program plays rock paper scissors with the computer
+ *
+ * By:      Michael Clermont
+ * Version: 1.0
+ * Since:   2022-11-25
+ */
+
+import promptSync from 'prompt-sync'
+
+const prompt = promptSync()
+const userInput = prompt('Enter the selection (rock,paper,scissors): ')
+const random = Math.floor(Math.random() * 3)
+const userLower = userInput.toLowerCase()
+let computerInput
+if (random === 1) {
+  computerInput = 'scissors'
+} else if (random === 2) {
+  computerInput = 'rock'
+} else if (random === 3) {
+  computerInput = 'paper'
+}
+if (computerInput === userLower) {
+  console.log('You won aganist the computer!')
+} else {
+  console.log('You lost aganist the computer!')
+}
+
+console.log('\nDone.')
